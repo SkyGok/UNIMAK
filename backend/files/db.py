dd7c635b86ce28e77aa5315856c06a115e2c5f74
Gokhan <gokhanskydulger@gmail.com>
2025-09-03 11:06:58 +0300

    adjusted the db but we're still behind for demo

--- backend/files/db.py
+++ backend/files/db.py
@@ -27,6 +27,14 @@ CREATE TABLE IF NOT EXISTS managers (
 )
 """)
 
+# Engineers table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS engineers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    engineer_name TEXT NOT NULL UNIQUE
+)
+""")
+
 # Customers table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS customers (
@@ -41,13 +49,34 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS projects (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     project_number TEXT NOT NULL UNIQUE,
+    project_name TEXT NOT NULL UNIQUE,
     manager_id INTEGER NOT NULL,
-    customer_id INTEGER NOT NULL,
     quantity INTEGER NOT NULL,
-    machine_type TEXT NOT NULL,
-    machine_top_group TEXT NOT NULL,
+    customer_id INTEGER NOT NULL,
     FOREIGN KEY (manager_id) REFERENCES managers(id),
     FOREIGN KEY (customer_id) REFERENCES customers(id)
+)
+""")
+
+# Groups table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS groups (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_id INTEGER NOT NULL,
+    engineer_id INTEGER NOT NULL,
+    component_no TEXT NOT NULL,
+    component_name TEXT NOT NULL,
+    unit_quantity INTEGER NOT NULL,
+    total_quantity INTEGER NOT NULL,
+    weight FLOAT, -- can be null
+    description TEXT, -- can be null
+    size TEXT, -- can be null
+    material TEXT NOT NULL,
+    machine_type TEXT NOT NULL,
+    notes TEXT, -- can be null
+    working_area TEXT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (engineer_id) REFERENCES engineers(id)
 )
 """)
 
@@ -56,14 +85,17 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS problems (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     project_id INTEGER NOT NULL,
+    group_id INTEGER NOT NULL,
     df_number TEXT UNIQUE NOT NULL,       -- latest df report
     recorder_id INTEGER NOT NULL,
     reason TEXT NOT NULL,
     description TEXT,
     photos_id TEXT,
-    status TEXT DEFAULT 'open',           -- open / closed
+    status TEXT DEFAULT 'waiting',        -- 11 different statuses possible
     record_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY(project_id) REFERENCES projects(id)
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (group_id) REFERENCES groups(id),
+    FOREIGN KEY (recorder_id) REFERENCES users(id)
 )
 """)
 
@@ -75,7 +107,7 @@ CREATE TABLE IF NOT EXISTS problem_steps (
     step_number INTEGER NOT NULL,
     df_filename TEXT NOT NULL,           -- e.g., df_1.xlsx, df_2.xlsx
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY(problem_id) REFERENCES problems(id)
+    FOREIGN KEY (problem_id) REFERENCES problems(id)
 )
 """)
 
